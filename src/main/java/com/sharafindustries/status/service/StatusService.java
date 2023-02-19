2f8049d79e12b67bf576eb0d4c1347fb3fdf04dd
Sharaf <sharaf@atwah.com>
2023-02-18 23:58:03 -0600

    WIP changes needed to Status object handling

--- src/main/java/com/sharafindustries/status/service/StatusService.java
+++ src/main/java/com/sharafindustries/status/service/StatusService.java
@@ -11,6 +11,8 @@ import com.sharafindustries.status.model.Status;
 @Service
 public class StatusService
 {
+	//TODO these need to be saved. maybe have these 3 accessible to everyone and field in user is only custom statuses?
+	//TODO also maybe add name field
 	public static final Status DEFAULT_AVAILABLE_STATUS = new Status("Available", "I'm available right now!");
 	public static final Status DEFAULT_AWAY_STATUS = new Status("Away", "I'm away from my phone a while");
 	public static final Status DEFAULT_BUSY_STATUS = new Status("Busy", "Sorry, I can't talk right now");
