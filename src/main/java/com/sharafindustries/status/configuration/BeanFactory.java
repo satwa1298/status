3f3d41b59e3298bf77a726be833aa494fc5f2fbb
Sharaf Atwa <sharaf.atwa@fdmgroup.com>
2023-02-22 10:09:43 -0600

    added prototype scope to createUser bean method

--- src/main/java/com/sharafindustries/status/configuration/BeanFactory.java
+++ src/main/java/com/sharafindustries/status/configuration/BeanFactory.java
@@ -3,6 +3,7 @@ package com.sharafindustries.status.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import com.sharafindustries.status.model.User;
 import com.sharafindustries.status.service.StatusService;
@@ -14,6 +15,7 @@ public class BeanFactory
 	private StatusService statusService;
 	
 	@Bean
+	@Scope("prototype")
 	public User createUser(String email)
 	{
 		User user = new User(email);
